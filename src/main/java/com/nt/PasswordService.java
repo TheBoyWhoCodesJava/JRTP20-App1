ea80ba617f0b70b9d20c66337fe73253f7177b01
TheBoyWhoDecodes <peyifih962@giratex.com>
2024-02-14 00:18:59 +0530

    changes done

--- src/main/java/com/nt/PasswordService.java
+++ src/main/java/com/nt/PasswordService.java
@@ -6,7 +6,7 @@ import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
 
 public class PasswordService {
-	
+	// This is important program
 	public static String encode(String txt) {
 		
 		Encoder encoder=Base64.getEncoder();
